f7ac7ccb8ad2b43eec1aa5ba682865399bc0fb4a
yara-fe <yarafe95@gmail.com>
2023-06-17 10:24:24 +1000

    first half of the weather.py answered

--- run_tests.py
+++ run_tests.py
@@ -6,16 +6,16 @@ from tests.test_load_data_from_csv import LoadCSVTests
 from tests.test_find_min import FindMinTests
 from tests.test_find_max import FindMaxTests
 from tests.test_generate_summary import GenerateSummaryTests
-from tests.test_generate_daily_summary import GenerateDailySummaryTests
+# from tests.test_generate_daily_summary import GenerateDailySummaryTests
 
 runner = unittest.TextTestRunner()
 
 print("Running Tests...\n")
-runner.run(unittest.TestSuite((unittest.makeSuite(ConvertDateTests))))
-runner.run(unittest.TestSuite((unittest.makeSuite(ConvertTempTests))))
-runner.run(unittest.TestSuite((unittest.makeSuite(CalculateMeanTests))))
-runner.run(unittest.TestSuite((unittest.makeSuite(LoadCSVTests))))
-runner.run(unittest.TestSuite((unittest.makeSuite(FindMinTests))))
-runner.run(unittest.TestSuite((unittest.makeSuite(FindMaxTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(ConvertDateTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(ConvertTempTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(CalculateMeanTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(LoadCSVTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(FindMinTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(FindMaxTests))))
 runner.run(unittest.TestSuite((unittest.makeSuite(GenerateSummaryTests))))
-runner.run(unittest.TestSuite((unittest.makeSuite(GenerateDailySummaryTests))))
+# runner.run(unittest.TestSuite((unittest.makeSuite(GenerateDailySummaryTests))))
--- weather.py
+++ weather.py
@@ -24,7 +24,13 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-    pass
+    #create a date object with iso_string
+    date_object = datetime.strptime(iso_string, "%Y-%m-%dT%H:%M:%S%z")
+
+    #convert the date_object to the new format
+    date_new = datetime.strptime(date_object, "%A %d %B %Y")
+
+    return date_new
 
 
 def convert_f_to_c(temp_in_farenheit):
@@ -35,7 +41,15 @@ def convert_f_to_c(temp_in_farenheit):
     Returns:
         A float representing a temperature in degrees celcius, rounded to 1dp.
     """
-    pass
+    # print (type(temp_in_farenheit))
+
+    #convert input into a float
+    temp_float = float(temp_in_farenheit)
+
+    #convert temp from F to C, round to 1 dp
+    degrees_c = round(((temp_float-32)*5/9),1)
+
+    return degrees_c
 
 
 def calculate_mean(weather_data):
@@ -46,8 +60,18 @@ def calculate_mean(weather_data):
     Returns:
         A float representing the mean value.
     """
-    pass
+    sum_of_list = 0
 
+    # convert the elements within weather_data to float
+    float_weather_data = [float(i) for i in weather_data]
+    
+    # get the sum of the elements
+    for i in range(len(float_weather_data)):
+        sum_of_list += float_weather_data[i]
+
+    # calculate the mean 
+    mean = sum_of_list/len(float_weather_data)
+    return mean
 
 def load_data_from_csv(csv_file):
     """Reads a csv file and stores the data in a list.
@@ -66,9 +90,32 @@ def find_min(weather_data):
     Args:
         weather_data: A list of numbers.
     Returns:
-        The minium value and it's position in the list.
+        The minium value and its position in the list.
     """
-    pass
+    #check if the list is empty
+    if not weather_data:
+        return ()
+
+    #convert each temperature in the list to a float
+    weather_data = [float(temp) for temp in (weather_data)]
+    # print(weather_data)
+        
+    # find the minimum in the list
+    minimum = min(weather_data)
+
+    # handle duplicates of minimum value
+    # create a new list called min_positions that contains the indices of elements in weather_data, 
+    # where the element is equal to the minimum value
+    min_positions = [index for index, temp in enumerate(weather_data) if temp == minimum]
+  
+    if len(min_positions) > 1:
+        #the minimum position will be the last occurence
+        position = min_positions[-1]
+    else:
+        position = min_positions[0]
+
+    #display the minimum value and its position
+    return (minimum, position)
 
 
 def find_max(weather_data):
@@ -77,9 +124,30 @@ def find_max(weather_data):
     Args:
         weather_data: A list of numbers.
     Returns:
-        The maximum value and it's position in the list.
+        The maximum value and its position in the list.
     """
-    pass
+    # check if the list is empty
+    if not weather_data:
+        return ()
+    
+    # convert each temperature in the list to a float
+    weather_data = [float(temp) for temp in (weather_data)]
+
+    # find maximum value
+    maximum = max(weather_data)
+    
+    # find all positions of the maximum value within the list.
+    # this will handle instances where maximum value appears more than once.
+    max_positions = [index for index, temp in enumerate(weather_data) if temp == maximum]
+    
+    #determines what position to use if there are multiple occurrences of max value
+    if len(max_positions) > 1:
+        position = max_positions[-1] #if more than one, use last occurrence
+    else:
+        position = max_positions[0]
+   
+    #return the maximum value and its position
+    return (maximum,position)
 
 
 def generate_summary(weather_data):
@@ -90,8 +158,35 @@ def generate_summary(weather_data):
     Returns:
         A string containing the summary information.
     """
-    pass
+    dates = []
+    min_temps = []
+    max_temps = []
 
+    for data in weather_data:
+        format_date = datetime.strptime(data[0], "%A %d %B %Y")
+        dates.append(format_date)
+        min_temps.append(data[1])
+        max_temps.append(data[2])
+
+    # find the minimum value
+    min_temp = min(min_temps)
+    max_temp = max(max_temps)
+    
+    
+    summary += f"({dates})"
+    summary += f" Minimum temperature: {min_temp}"
+    summary += f" Maximum temperature: {max_temp}"
+
+    return summary
+
+
+
+
+"""
+---- Friday 02 July 2021 ----
+  Minimum Temperature: 9.4°C
+  Maximum Temperature: 19.4°C        
+"""
 
 def generate_daily_summary(weather_data):
     """Outputs a daily summary for the given weather data.
